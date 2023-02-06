e35d2a9ca313e33e5475be6bb6232d4cbc517ec5
Peridot <peridot491@protonmail.com>
2023-02-06 22:03:59 +0100

    Update license

--- bukkit/src/main/java/pl/spaceis/plugin/bukkit/BukkitSpaceIsCommand.java
+++ bukkit/src/main/java/pl/spaceis/plugin/bukkit/BukkitSpaceIsCommand.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2023 Kamil Trysiński
+ * Copyright (C) 2022 SpaceIs-plugin Contributors
+ * https://github.com/SpaceCodePoland/spaceis-plugin/graphs/contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,18 +17,12 @@
 
 package pl.spaceis.plugin.bukkit;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Locale;
-import java.util.Set;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
 import pl.spaceis.plugin.command.SpaceIsCommand;
 import pl.spaceis.plugin.config.Config;
-import pl.spaceis.plugin.config.EmptyConfigFieldException;
 import pl.spaceis.plugin.config.Messages;
-import pl.spaceis.plugin.resource.ResourceLoaderException;
 
 public class BukkitSpaceIsCommand implements CommandExecutor, SpaceIsCommand<CommandSender, String> {
 
--- common/src/main/java/pl/spaceis/plugin/request/SpaceIsRequest.java
+++ common/src/main/java/pl/spaceis/plugin/request/SpaceIsRequest.java
@@ -1,5 +1,6 @@
 /*
- * Copyright (C) 2022 Kamil Trysiński
+ * Copyright (C) 2022 SpaceIs-plugin Contributors
+ * https://github.com/SpaceCodePoland/spaceis-plugin/graphs/contributors
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
